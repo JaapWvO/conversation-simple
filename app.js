5fbf03334ee36a47e2e9467f22ebd9d795c0675a
Jaap Van Otten <jw_van_otten@nl.ibm.com>
2017-03-27 15:22:50 -0500

    more testing

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ if (!process.env.WORKSPACE_ID || process.env.WORKSPACE_ID === '<workspace-id>' )
   }
 
   var payload = {
-    workspace_id: process.env.WORKSPACE_BANK,
+    workspace_id: process.env.WORKSPACE_ID,
     context: {},
     input: {},
     alternate_intents: true
@@ -80,7 +80,7 @@ if (!process.env.WORKSPACE_ID || process.env.WORKSPACE_ID === '<workspace-id>' )
     if ( req.body.workspace ) {
       if (req.workspace === "ws_du_bank") { payload.workspace_id = process.env.WORKSPACE_BANK; }
       else if (req.workspace === "ws_en_driver") { payload.workspace_id = process.env.WORKSPACE_DLC; }
-      else payload.workspace_id = process.env.WORKSPACE_ID ;
+      else payload.workspace_id = process.env.WORKSPACE_BANK ;
     }
     if ( req.body.input ) {
       payload.input = req.body.input;
